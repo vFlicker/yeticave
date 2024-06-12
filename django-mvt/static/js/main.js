0cb8f84d03a6c39d47f96c0ff881b09693929ca7
Vladyslav Sliusar <flickervladislav@gmail.com>
2024-06-12 17:33:59 +0300

    feat: add subscriptions to menu

--- django-mvt/static/js/main.js
+++ django-mvt/static/js/main.js
@@ -1,10 +1,10 @@
 /** Menu toggler */
 (() => {
-  const buttonElement = document.querySelector(".button__menu-open");
-  const actionElement = document.querySelector(".user-menu--login");
+  const buttonElement = document.querySelector(".menu-toggler");
+  const actionElement = document.querySelector(".user-menu--logged-in");
 
   const onButtonClick = () => {
-    buttonElement.classList.toggle("menu-open--active");
+    buttonElement.classList.toggle("menu-toggler--active");
     actionElement.classList.toggle("user-menu--active");
   };
 
