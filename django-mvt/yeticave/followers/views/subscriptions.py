118620d7a8f0402bb8c65b5e931219d48c476c83
Vladyslav Sliusar <flickervladislav@gmail.com>
2024-06-12 18:07:50 +0300

    feat: add pagination

--- django-mvt/yeticave/followers/views/subscriptions.py
+++ django-mvt/yeticave/followers/views/subscriptions.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
@@ -11,11 +12,18 @@ from yeticave.lots.models.Comment import Comment
 @require_http_methods(["GET"])
 def subscriptions(request: AuthenticatedHttpRequest) -> HttpResponse:
     TEMPLATE = "followers/subscriptions.html"
+    COMMENTS_PER_PAGE = 5
+    DEFAULT_PAGE = 1
 
     profile_user = request.user
 
+    comments = Comment.objects.get_subscriptions_comments(profile_user)
+    paginator = Paginator(comments, COMMENTS_PER_PAGE)
+    page_number = request.GET.get("page", DEFAULT_PAGE)
+    page = paginator.get_page(page_number)
+
     context = {
-        "comments": Comment.objects.get_subscriptions_comments(profile_user),
+        "page": page,
     }
 
     return render(request, TEMPLATE, context)
--- django-mvt/yeticave/users/views/profile.py
+++ django-mvt/yeticave/users/views/profile.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_http_methods
@@ -11,6 +12,8 @@ User = get_user_model()
 @require_http_methods(["GET"])
 def profile(request: HttpRequest, username: str) -> HttpResponse:
     TEMPLATE = "users/profile.html"
+    COMMENTS_PER_PAGE = 5
+    DEFAULT_PAGE = 1
 
     profile_user = get_object_or_404(User, username=username)
     is_followed = False
@@ -18,7 +21,13 @@ def profile(request: HttpRequest, username: str) -> HttpResponse:
     if auth_user := get_authenticated_user(request):
         is_followed = Follow.objects.is_followed(auth_user, profile_user)
 
+    comments = profile_user.comments.get_all_recent()
+    paginator = Paginator(comments, COMMENTS_PER_PAGE)
+    page_number = request.GET.get("page", DEFAULT_PAGE)
+    page = paginator.get_page(page_number)
+
     context = {
+        "page": page,
         "profile_user": profile_user,
         "is_followed": is_followed,
     }
