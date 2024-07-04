ab4c2d2deeca37b1eab6ee19055868b0abd35251
Vladyslav Sliusar <flickervladislav@gmail.com>
2024-07-04 20:12:35 +0300

    chore: add eslint import sort plugin

--- express-app/eslint.config.js
+++ express-app/eslint.config.js
@@ -1,6 +1,8 @@
 import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
@@ -8,6 +10,20 @@ export default [
   eslintPluginPrettierRecommended,
 
   {
-    rules: {},
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
   },
 ];
