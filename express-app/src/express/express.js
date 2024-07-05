8692ff5247cb89875e3f32ceb4f3f089d081d75c
Vladyslav Sliusar <flickervladislav@gmail.com>
2024-07-05 19:36:12 +0300

    feat: add front server

--- express-app/src/express/express.js
+++ express-app/src/express/express.js
@@ -1,9 +1,26 @@
 import chalk from 'chalk';
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import { ExitCode, FRONT_PORT } from '../constants.js';
 
+const PUBLIC_DIR = 'public';
+const TEMPLATES_DIR = 'templates';
+
 const app = express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(import.meta.dirname, TEMPLATES_DIR));
+
+app.use(express.static(path.join(__dirname, PUBLIC_DIR)));
+
+app.get('/', (req, res) => {
+  res.render('pages/index');
+});
 
 app.listen(FRONT_PORT, (err) => {
   if (err) {
